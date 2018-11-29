5465fece5691b32b901acb4b8a22ab26021116e8
meagantrenchard <meagan.trenchard@gmail.com>
2018-11-29 12:50:02 -0500

    update

---
+++ project/collection/main.js
@@ -0,0 +1,3 @@
+<i class="fas fa-stroopwafel"></i> <!-- this icon's 1) style prefix == fas and 2) icon name == stroopwafel -->
+<i class="fas fa-stroopwafel"></i> <!-- using an <i> element to reference the icon -->
+<span class="fas fa-stroopwafel"></span> <!-- using a <span> element to reference the icon -->
