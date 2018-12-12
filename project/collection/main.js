8ec580c0ba568da523700ec1151aeb3242291b2b
meagantrenchard <meagan.trenchard@gmail.com>
2018-12-11 19:51:43 -0500

    udpate

--- project/collection/main.js
+++ project/collection/main.js
@@ -1,10 +1,8 @@
-<script>
-  // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
-  // On click
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
-  });
-</script>
+function myFunction(){
+	var x = document.getElementById('myTopnav');
+	if (x.className ==='nav'){
+		x.className += ' responsive';
+	}else{
+		x.className= 'nav';
+	}
+}
