b6b6526651640add1fcbe1b7e40d496ab9860f9e
meagantrenchard <meagan.trenchard@gmail.com>
2018-12-12 00:36:21 -0500

    update

--- project/collection/main.js
+++ project/collection/main.js
@@ -1,8 +1,8 @@
-function myFunction(){
-	var x = document.getElementById('myTopnav');
-	if (x.className ==='nav'){
-		x.className += ' responsive';
-	}else{
-		x.className= 'nav';
-	}
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
 }
